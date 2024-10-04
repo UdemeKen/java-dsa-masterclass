fa415f1abc35b2d2bdf898838970f0414ec6785c
UdemeKen <udemekendrick@gmail.com>
2024-10-04 19:48:24 +0100

    Insertion array added

--- src/main/java/org/udemekendrick/Main.java
+++ src/main/java/org/udemekendrick/Main.java
@@ -1,7 +1,14 @@
 package org.udemekendrick;
 
+import org.udemekendrick.arrays.SingleDimensionalArray;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SingleDimensionalArray array = new SingleDimensionalArray(5);
+        array.insert(0, 1);
+        array.insert(1, 2);
+        array.insert(2, 3);
+        array.insert(1, 4);
+        array.insert(10, 5);
     }
 }
