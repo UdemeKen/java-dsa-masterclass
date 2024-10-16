f30c3af92450e20e5fe7fe5a6b01ddc628188e6d
decagon <udemekendrick@gmail.com>
2024-10-16 16:32:36 +0100

    two dimensional array completed

--- src/main/java/org/udemekendrick/Main.java
+++ src/main/java/org/udemekendrick/Main.java
@@ -1,17 +1,25 @@
 package org.udemekendrick;
 
 import org.udemekendrick.arrays.SingleDimensionalArray;
+import org.udemekendrick.arrays.TwoDimensionalArray;
+
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        SingleDimensionalArray array = new SingleDimensionalArray(5);
-        array.insert(0, 1);
-        array.insert(1, 2);
-        array.insert(2, 3);
-        array.insert(1, 4);
-        array.insert(10, 5);
+        TwoDimensionalArray twoDimensionalArray = new TwoDimensionalArray(3, 3);
+        twoDimensionalArray.insertValueInTheArray(0, 0, 10);
+        twoDimensionalArray.insertValueInTheArray(0,1, 20);
+        twoDimensionalArray.insertValueInTheArray(0,2, 30);
+        twoDimensionalArray.insertValueInTheArray(1,0, 40);
+        twoDimensionalArray.insertValueInTheArray(1,1, 50);
+        twoDimensionalArray.insertValueInTheArray(1,2, 60);
+        twoDimensionalArray.insertValueInTheArray(2,0, 70);
+        twoDimensionalArray.insertValueInTheArray(2,1, 80);
+        twoDimensionalArray.insertValueInTheArray(2,2, 90);
 
-        System.out.println("Search in array!");
-        array.deleteElement(1);
+        System.out.println(Arrays.deepToString(twoDimensionalArray.arr));
+        twoDimensionalArray.deleteValueFromArray(1, 1);
+        System.out.println(Arrays.deepToString(twoDimensionalArray.arr));
     }
 }
--- src/main/java/org/udemekendrick/arrays/TwoDimensionalArray.java
+++ src/main/java/org/udemekendrick/arrays/TwoDimensionalArray.java
@@ -10,14 +10,66 @@ package org.udemekendrick.arrays;
  * @since 10/15/2024
  */
 public class TwoDimensionalArray {
-    int arr[][] = null;
+    public int arr[][] = null;
 
     public TwoDimensionalArray(int numberOfRows, int numberOfColumns) {
         this.arr = new int[numberOfRows][numberOfColumns];
-        for (int row = 0; row < arr[0].length; row++) {
+        for (int row = 0; row < arr.length; row++) {
             for (int col = 0; col < arr[0].length; col++) {
                 arr[row][col] = Integer.MIN_VALUE;
             }
+        }
+    }
+
+    public void insertValueInTheArray(int row, int col, int value) {
+        try {
+            if (arr[row][col] == Integer.MIN_VALUE) {
+                arr[row][col] = value;
+                System.out.println("The value is successfully inserted!");
+            } else  {
+                System.out.println("This cell is already occuppied!");
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid index for 2D array");
+        }
+    }
+
+    public void accessCell(int row, int col) {
+        System.out.println("\nAccessing Row: " + row + ", Col: " + col);
+        try {
+            System.out.println("The value of the cell is " + arr[row][col]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid index for 2D array.");
+        }
+    }
+
+    public void traverse2DArray() {
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[0].length; col++) {
+                System.out.print(arr[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void searchingValue(int value) {
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[0].length; col++) {
+                if (arr[row][col] == value) {
+                    System.out.println("The value is found at row: " + row + " and at col: " + col);
+                    return;
+                }
+            }
+        }
+        System.out.println("The value is not found");
+    }
+
+    public void deleteValueFromArray(int row, int col) {
+        try {
+            System.out.println("Successfully deleted at row: " + row + " col " + col);
+            arr[row][col] = Integer.MIN_VALUE;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("This index is not valid for an array");
         }
     }
 }
