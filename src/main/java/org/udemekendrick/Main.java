5b7807dd233450e07a5ec720ab1f917155b12a88
decagon <udemekendrick@gmail.com>
2024-11-04 10:06:21 +0100

    middle number added

--- src/main/java/org/udemekendrick/Main.java
+++ src/main/java/org/udemekendrick/Main.java
@@ -1,10 +1,15 @@
 package org.udemekendrick;
 
+import org.udemekendrick.arrays.exercises.ExerciseOne;
 import org.udemekendrick.arrays.projects.AverageTemperature;
+
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        AverageTemperature averageTemperature = new AverageTemperature();
-        averageTemperature.calculateAverageTemperature();
+        ExerciseOne exerciseOne = new ExerciseOne();
+        int[] array = {1, 2, 3, 4};
+        int[] result = exerciseOne.middle(array);
+        System.out.println(Arrays.toString(result));
     }
 }
---
+++ src/main/java/org/udemekendrick/arrays/exercises/ExerciseOne.java
@@ -0,0 +1,15 @@
+package org.udemekendrick.arrays.exercises;
+
+// Write a function called middle that takes an array and returns a new array that contains all but the first and last elements.
+
+public class ExerciseOne {
+
+    public int[] middle(int[] array) {
+        int size = array.length - 2;
+        int[] newArray = new int[size];
+        for (int i = 1; i < array.length - 1; i++) {
+            newArray[i - 1] = array[i];
+        }
+        return newArray;
+    }
+}
