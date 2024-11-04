8243a90da20a30ed26cee8020bda0caae1d1bc84
decagon <udemekendrick@gmail.com>
2024-11-04 17:26:42 +0100

    exercise six added

--- src/main/java/org/udemekendrick/Main.java
+++ src/main/java/org/udemekendrick/Main.java
@@ -2,14 +2,15 @@ package org.udemekendrick;
 
 import org.udemekendrick.arrays.exercises.ExerciseFive;
 import org.udemekendrick.arrays.exercises.ExerciseFour;
+import org.udemekendrick.arrays.exercises.ExerciseSix;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        ExerciseFive exerciseFive = new ExerciseFive();
+        ExerciseSix exerciseSix = new ExerciseSix();
         int[] array = {4, 5, 6, 6, 7, 9, 9, 3, 8, 8};
-        int[] result = exerciseFive.removeDuplicates(array);
-        System.out.println(Arrays.toString(result));
+        int result = exerciseSix.removeDuplicatesTwo(array);
+        System.out.println(result);
     }
 }
---
+++ src/main/java/org/udemekendrick/arrays/exercises/ExerciseSix.java
@@ -0,0 +1,21 @@
+package org.udemekendrick.arrays.exercises;
+
+// Given a sorted array nums, remove the duplicates in-place such that each element appears only once and returns the new length.
+// Do not allocate extra space for another array; you must do this by modifying the new input array in place with 0(1)
+
+public class ExerciseSix {
+
+    public int removeDuplicatesTwo(int[] array) {
+        if (array.length == 0) {
+            return 0;
+        }
+        int uniqueIndex = 1;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] != array[i - 1]) {
+                array[uniqueIndex] = array[i];
+                uniqueIndex++;
+            }
+        }
+        return uniqueIndex;
+    }
+}
