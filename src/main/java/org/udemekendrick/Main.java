fb0cb6edc43190d1f400a244e330df2f07db8ccf
decagon <udemekendrick@gmail.com>
2024-11-04 12:08:08 +0100

    exercise four added

--- src/main/java/org/udemekendrick/Main.java
+++ src/main/java/org/udemekendrick/Main.java
@@ -1,17 +1,12 @@
 package org.udemekendrick;
 
-import org.udemekendrick.arrays.exercises.ExerciseOne;
-import org.udemekendrick.arrays.exercises.ExerciseThree;
-import org.udemekendrick.arrays.exercises.ExerciseTwo;
-import org.udemekendrick.arrays.projects.AverageTemperature;
-
-import java.util.Arrays;
+import org.udemekendrick.arrays.exercises.ExerciseFour;
 
 public class Main {
     public static void main(String[] args) {
-        ExerciseThree exerciseThree = new ExerciseThree();
-        int[] array = {9, 8, 7, 6, 5};
-        int[] result = exerciseThree.findTopTwoScores(array);
-        System.out.println(Arrays.toString(result));
+        ExerciseFour exerciseFour = new ExerciseFour();
+        int[] array = {4, 5, 6, 7, 9};
+        int result  = exerciseFour.findMissingNumberInArray(array);
+        System.out.println(result);
     }
 }
---
+++ src/main/java/org/udemekendrick/arrays/exercises/ExerciseFour.java
@@ -0,0 +1,17 @@
+package org.udemekendrick.arrays.exercises;
+
+// Write a Java function called "findMissingNumberInArray" that takes an integer array containing n-1 unique elements from a range of 1 to n, with one missing number,
+// and returns the missing number.
+
+public class ExerciseFour {
+
+    public int findMissingNumberInArray (int[] array) {
+        int n = array.length + 1;
+        int expectedSum = (n * (n + 1)) / 2;
+        int actualSum = 0;
+        for (int num : array) {
+            actualSum += num;
+        }
+        return expectedSum - actualSum;
+    }
+}
