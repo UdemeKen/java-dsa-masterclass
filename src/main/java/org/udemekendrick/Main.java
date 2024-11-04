4e251f65d8dd8449e65cb60c57cbf08c073dbb70
decagon <udemekendrick@gmail.com>
2024-11-04 17:45:37 +0100

    exercise seven added

--- src/main/java/org/udemekendrick/Main.java
+++ src/main/java/org/udemekendrick/Main.java
@@ -2,15 +2,16 @@ package org.udemekendrick;
 
 import org.udemekendrick.arrays.exercises.ExerciseFive;
 import org.udemekendrick.arrays.exercises.ExerciseFour;
+import org.udemekendrick.arrays.exercises.ExerciseSeven;
 import org.udemekendrick.arrays.exercises.ExerciseSix;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        ExerciseSix exerciseSix = new ExerciseSix();
-        int[] array = {4, 5, 6, 6, 7, 9, 9, 3, 8, 8};
-        int result = exerciseSix.removeDuplicatesTwo(array);
+        ExerciseSeven exerciseSeven = new ExerciseSeven();
+        int[] prices = {4, -5, 6, 6, -7, 9, 9, -3, -8, 8, -1000};
+        int result = exerciseSeven.maxProfit(prices);
         System.out.println(result);
     }
 }
---
+++ src/main/java/org/udemekendrick/arrays/exercises/ExerciseSeven.java
@@ -0,0 +1,24 @@
+package org.udemekendrick.arrays.exercises;
+
+// You are given an array "prices" where "price[i]" is the price of a given stock on the ith day. You want to maximize your profit by choosing a single day to buy
+// one stock and choosing a different day in the future to sell that stock. Return the maximum profit you can achieve from this transaction. If you can not achieve
+// any transaction return 0.
+
+public class ExerciseSeven {
+
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+        int minPrice = Integer.MIN_VALUE;
+        int maxPrice = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else if (prices[i] - minPrice > maxPrice) {
+                maxPrice = prices[i] - minPrice;
+            }
+        }
+        return maxPrice;
+    }
+}
