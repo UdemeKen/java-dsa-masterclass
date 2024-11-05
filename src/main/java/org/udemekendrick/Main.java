7012e2c58565d1227a54f699e6c9058fc38919db
decagon <udemekendrick@gmail.com>
2024-11-05 06:30:24 +0100

    exercise ten added

--- src/main/java/org/udemekendrick/Main.java
+++ src/main/java/org/udemekendrick/Main.java
@@ -6,10 +6,9 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        ExerciseNine exerciseNine = new ExerciseNine();
-        int[] array = {4, -5, 6, 6, -7, 9, 9, -3, -8, 8, -1000};
-        int valueToSearch = 6;
-        int result = exerciseNine.searchInArray(array, valueToSearch);
+        ExerciseTen exerciseTen = new ExerciseTen();
+        int[] array = {4, 5, 6, 7, 9, 3, 8,};
+        String result = exerciseTen.maxProduct(array);
         System.out.println(result);
     }
 }
---
+++ src/main/java/org/udemekendrick/arrays/exercises/ExerciseTen.java
@@ -0,0 +1,20 @@
+package org.udemekendrick.arrays.exercises;
+
+// How to find maximum product of two integers in the array where elements are positive.
+
+public class ExerciseTen {
+
+    public String maxProduct(int[] array) {
+        int maxProduct = 0;
+        String pairs = "";
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] * array[j] > maxProduct) {
+                    maxProduct = array[i] * array[j];
+                    pairs = Integer.toString(array[i]) + ", " + Integer.toString(array[j]);
+                }
+            }
+        }
+        return pairs;
+    }
+}
