8f3b954e36f40c0661004341acd22c3802c52c5c
decagon <udemekendrick@gmail.com>
2024-11-05 06:18:33 +0100

    exercise nine added

--- src/main/java/org/udemekendrick/Main.java
+++ src/main/java/org/udemekendrick/Main.java
@@ -6,10 +6,10 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        ExerciseEight exerciseEight = new ExerciseEight();
-        int[] nums = {4, -5, 6, 6, -7, 9, 9, -3, -8, 8, -1000};
-        int target = 6;
-        int[] result  = exerciseEight.twoSum(nums, target);
-        System.out.println(Arrays.toString(result));
+        ExerciseNine exerciseNine = new ExerciseNine();
+        int[] array = {4, -5, 6, 6, -7, 9, 9, -3, -8, 8, -1000};
+        int valueToSearch = 6;
+        int result = exerciseNine.searchInArray(array, valueToSearch);
+        System.out.println(result);
     }
 }
