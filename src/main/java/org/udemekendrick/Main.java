fa2fffccb0de40c4db61aacbc8535da36e1b0680
decagon <udemekendrick@gmail.com>
2024-11-06 14:38:11 +0100

    exercise fourteen added

--- src/main/java/org/udemekendrick/Main.java
+++ src/main/java/org/udemekendrick/Main.java
@@ -1,10 +1,12 @@
 package org.udemekendrick;
 
+import org.udemekendrick.arrays.exercises.ExerciseFourteen;
 import org.udemekendrick.arrays.exercises.ExerciseThirteen;
 
 public class Main {
     public static void main(String[] args) {
-        ExerciseThirteen exerciseThirteen = new ExerciseThirteen();
-        int[][] array1 = {{4, 5, 6, 7, 9, 3, 8,}, {4, 5, 6, 7, 9, 3, 8,}, {4, 5, 6, 7, 9, 3, 8,}};
+        ExerciseFourteen exerciseFourteen = new ExerciseFourteen();
+        int[] array1 = {4, 5, 6, 7, 9, 3, 8,};
+        exerciseFourteen.sumProductOfArray(array1);
     }
 }
---
+++ src/main/java/org/udemekendrick/arrays/exercises/ExerciseFourteen.java
@@ -0,0 +1,19 @@
+package org.udemekendrick.arrays.exercises;
+
+// Create a function which calculate the sum and products of elements of array.
+// Find the time complexity for created method.
+
+public class ExerciseFourteen {
+
+    public void sumProductOfArray(int[] array) {
+        int sum = 0;
+        int product = 1;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        for (int i = 0; i < array.length; i++) {
+            product *= array[i];
+        }
+        System.out.println(sum + ", " + product);
+    }
+}
