90b94a62eac27d19f4a77a18180192d178bbcf9c
decagon <udemekendrick@gmail.com>
2024-11-07 11:18:48 +0100

    question seven added

--- src/main/java/org/udemekendrick/Main.java
+++ src/main/java/org/udemekendrick/Main.java
@@ -2,17 +2,19 @@ package org.udemekendrick;
 
 import org.udemekendrick.topTenCodingQuestions.QuestionFive;
 import org.udemekendrick.topTenCodingQuestions.QuestionFour;
+import org.udemekendrick.topTenCodingQuestions.QuestionSeven;
 import org.udemekendrick.topTenCodingQuestions.QuestionSix;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        QuestionSix questionSix = new QuestionSix();
-        String inputString = "omo";
+        QuestionSeven question = new QuestionSeven();
+        String inputString1 = "silent";
+        String inputString2 = "listen";
 //        int[] array1 = {4, 5, 6, 7, 9, 3, 8,};
 //        int[] array2 = {7, 3, 9, 4, 8, 1, 2,};
-        boolean result = questionSix.isPalindrome(inputString);
+        boolean result = question.anagrams(inputString1, inputString2);
         System.out.println(result);
     }
 }
