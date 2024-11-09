d5fb02f69568b8bf8e7895b768f51206cde10dc2
decagon <udemekendrick@gmail.com>
2024-11-09 08:27:35 +0100

    singly linked list created

--- src/main/java/org/udemekendrick/Main.java
+++ src/main/java/org/udemekendrick/Main.java
@@ -1,14 +1,14 @@
 package org.udemekendrick;
 
+import org.udemekendrick.linkedList.singlyLinkedList.SinglyLinkedList;
 import org.udemekendrick.topTenCodingQuestions.*;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        QuestionTen questionTen = new QuestionTen();
-        int[] num = {5, 1, 8, 4, 3, 7, 2};
-        questionTen.bubbleSortArray(num);
-        System.out.println(Arrays.toString(num));
+        SinglyLinkedList singlyLinkedList = new SinglyLinkedList();
+        singlyLinkedList.createSinglyLinkedList(5);
+        System.out.println(singlyLinkedList.head.value);
     }
 }
---
+++ src/main/java/org/udemekendrick/linkedList/Node.java
@@ -0,0 +1,6 @@
+package org.udemekendrick.linkedList;
+
+public class Node {
+    public int value;
+    public Node next;
+}
